e831b4518f64296254683c2bded97386c42ed978
edwincamirtharaj <edwincamirtharaj@gmail.com>
2024-02-27 12:34:19 +0000

    Changes in Menu of Dashboard Page

--- sfcy/twilio_integration/views.py
+++ sfcy/twilio_integration/views.py
@@ -11,8 +11,59 @@ from django.views.decorators.csrf import csrf_exempt
 def success_view(request):
     return render(request, 'twilio_integration/success.html')
 
+
+def send_whatsapp_message(to, body):
+    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+
+    message = client.messages.create(
+        body=body,
+        from_='whatsapp:' + settings.TWILIO_PHONE_NUMBER,
+        to='whatsapp:' + to
+    )
+
+    return message.sid
+
 @csrf_exempt
 def handle_whatsapp_message(request: HttpRequest):
+    # Extract data from Twilio request
+    incoming_message = request.POST.get('Body', '')
+    sender_whatsapp_number = request.POST.get('From', '')
+
+    cleaned_number = ''.join(c for c in sender_whatsapp_number if c.isdigit())[2:]
+    try:
+        whatsapp_number = WhatsAppNumber.objects.get(number=cleaned_number)
+        associated_companies = whatsapp_number.companies.all()
+
+        # Check if the user is associated with any companies
+        if associated_companies.exists():
+            # Prompt the user with their company list
+            menu_message = ""
+            for company in associated_companies:
+                menu_message += f"{company.name}\n"
+
+
+
+
+            # Send the company list as a reply
+            response = MessagingResponse()
+            response.message(menu_message)
+            return HttpResponse(str(response))
+
+        else:
+            # If the user is not associated with any companies, handle accordingly
+            response = MessagingResponse()
+            response.message("You are not associated with any companies.")
+            return HttpResponse(str(response))
+
+    except WhatsAppNumber.DoesNotExist:
+        # Handle the case when the WhatsApp number is not found in the database
+        response = MessagingResponse()
+        response.message("Your WhatsApp number is not registered.")
+        return HttpResponse(str(response))
+
+
+@csrf_exempt
+def handle_whatsapp_message_old(request: HttpRequest):
     # Extract data from Twilio request
     incoming_message = request.POST.get('Body', '')
     sender_whatsapp_number = request.POST.get('From', '')
@@ -50,16 +101,3 @@ def handle_whatsapp_message(request: HttpRequest):
         response = MessagingResponse()
         response.message("Your WhatsApp number is not registered.")
         return HttpResponse(str(response))
-
-
-
-def send_whatsapp_message(to, body):
-    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-
-    message = client.messages.create(
-        body=body,
-        from_='whatsapp:' + settings.TWILIO_PHONE_NUMBER,
-        to='whatsapp:' + to
-    )
-
-    return message.sid
